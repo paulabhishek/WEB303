2abdb0078f71feac3898331a87a4d39dde0eeb39
Abhishek Paul <32842796+paulabhishek@users.noreply.github.com>
2021-11-04 07:25:14 -0400

    Uploading assignment 5 files

--- script.js
+++ script.js
@@ -1,99 +1,87 @@
 /*
-    Assignment 4
-    {ABhishek Paul}
+    Assignment 5
+    {Abhishek Paul}
 */
 
 $(document).ready(function(){
-    var htmlEle= '';
+    class ContentCard{
+        id;
+        title;
+        description;
+        categoryType;
 
-    const success = (position) => {
-        //$('#youarehere').eq(0).html("The latitude is: " + position.coords.latitude);
-        $('#latitude').eq(0).html(`The latitude is: ${position.coords.latitude}`);
-        var $lat2 = position.coords.latitude;
-        console.log($lat2);
-        $('#longitude').eq(0).html(`The longitude is: ${position.coords.longitude}`);
-        var $lon2 = position.coords.longitude;
-        console.log($lon2);        
-        localStorage.setItem("lat1", position.coords.latitude);  
-        localStorage.setItem("lon1", position.coords.longitude);
-        console.log(position);
-
-    if (localStorage.getItem('lat1') && localStorage.getItem('lon1')  !== null) {
-        htmlEle += '<div id="localStoragelat1"> Location in localStorage lat: '+localStorage.getItem('lat1')+'</div>';
-        htmlEle += '<div id="localStoragelon1">Location in localStorage lon: '+localStorage.getItem('lon1')+'</div>';
-        $('#content').append(htmlEle); 
-        //console.log(calcDistance(localStorage.getItem('lat1'), localStorage.getItem('lon1'), $lat2, $lon2));
-        
-        var toRadians = function(num) {
-            htmlEle += '<div id=" "> The person has moved from: '+num * Math.PI / 180+'</div>';
-            $('#content').append(htmlEle); 
-            console.log( num * Math.PI / 180);
-            return num * Math.PI / 180;
-        }
-        var R = 6371000; // radius of Earth in metres
-        var φ1 = toRadians(localStorage.getItem('lat1'));
-        var φ2 = toRadians(position.coords.latitude);
-        var Δφ = toRadians(position.coords.latitude-localStorage.getItem('lat1'));
-        var Δλ = toRadians(position.coords.longitude-localStorage.getItem('lon1'));        
-        var φ1 = toRadians(localStorage.getItem('lat1'));
-
-
-        var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ/2) * Math.sin(Δλ/2);
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        console.log( R * c );
-        return ( R * c );        
-        }
-        else{
-            $('#content').eq(0).html(`Welcome!`);
+        constructor(id, title, description, categoryType){
+            this.id = id;
+            this.title = title;
+            this.description = description;
+            this.categoryType = categoryType;
+            //console.log(id);
         }
 
+        updateContent(id, title, description, categoryType){
+            if(id !== null || title !== null || description !== null || categoryType !== null){
+            this.id = id;
+            this.title = title;
+            this.description = description;
+            this.categoryType = categoryType;                
+            }
+        }
 
+    }
+    let movies = ['0','Eternals', 'The Eternals, a race of immortal beings with superhuman powers who have secretly lived on Earth ', 'Adventure/Action',
+                   '1','Dune','Paul Atreides, a brilliant and gifted young man born into a great destiny beyond his understanding', 'Sci-fi/Adventure',
+                   '2','No Time To Die','James Bond is enjoying a tranquil life in Jamaica after leaving active service.', 'Action/Adventure',
+                   '3','Last Night in Soho','An aspiring fashion designer is mysteriously able to enter the 1960s', 'Horror/Psychological horror',
+                   '4', 'Halloween Kills', 'The nightmare is not over as unstoppable killer Michael Myers escapes from Laurie Strodes trap', 'Horror/Thriller'
+                ];
+     console.log(movies.length);    
+    let content0 = new ContentCard(movies[0], movies[1], movies[2], movies[3]);
+    let contentList = document.getElementById('content-list');
 
+    let wrapper = document.createElement('div');
+    wrapper.setAttribute("id", "content-"+ content0.id);
 
-    };
-        const fail = (error) => {
-        $('#content').eq(0).html(`Geo Position not available!`); 
+    let header = document.createElement('h4');
+    header.textContent = content0.title;
 
+    let p = document.createElement('p');
+    p.textContent = content0.description;
 
+    let div = document.createElement('div');
+    div.textContent = content0.categoryType;
 
+    wrapper.append(header);
+    wrapper.append(p);
+    wrapper.append(div);
+    contentList.append(wrapper);
 
-
-    };
-    navigator.geolocation.getCurrentPosition(success, fail);
-
-
-    // function calcDistance(lat1, lon1, lat2, lon2){
-    //     var toRadians = function(num) {
-    //         return num * Math.PI / 180;
-    //         console.log( num * Math.PI / 180); 
-    //     }
-    //     var R = 6371000; // radius of Earth in metres
-    //     var φ1 = toRadians(lat1);
-    //     var φ2 = toRadians(lat2);
-    //     var Δφ = toRadians(lat2-lat1);
-    //     var Δλ = toRadians(lon2-lon1);
-
-
-        // var φ1 = toRadians(localStorage.getItem('lat1'));
-        // var φ2 = toRadians(position.coords.latitude);
-        // var Δφ = toRadians(position.coords.latitude-localStorage.getItem('lat1'));
-        // var Δλ = toRadians(position.coords.longitude-localStorage.getItem('lon1'));
-
-    //     var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ/2) * Math.sin(Δλ/2);
-    //     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
-    //     return ( R * c );
-
-    // }
-});
-
+    let temp = 4;
+    for (let i = 4; i < movies.length-4; i++) {
     
-    // your code here
+    content0.updateContent(movies[temp], movies[temp+1], movies[temp+2], movies[temp+3]);
+    temp = temp + 4;
+    //console.log(temp);
+    let contentList = document.getElementById('content-list');
 
+    let wrapper = document.createElement('div');
+    wrapper.setAttribute("id", "content-"+ content0.id);
 
+    let header = document.createElement('h4');
+    header.textContent = content0.title;
 
-    // function to calculate the distance in metres between two lat/long pairs on Earth
-    // Haversine formula - https://en.wikipedia.org/wiki/Haversine_formula
-    // Aren't those cool variable names? Yah gotta love JavaScript
+    let p = document.createElement('p');
+    p.textContent = content0.description;
+    //console.log(content.description);
+
+    let div = document.createElement('div');
+    div.textContent = content0.categoryType;
+
+    wrapper.append(header);
+    wrapper.append(p);
+    wrapper.append(div);
+    contentList.append(wrapper);
+    }
+});    
+
 
 
